f4246b73218645354734a6ee7b1a9e80561ebfa1
Eric Fry <yuv422@reversedgames.com>
2026-08-24 08:20:21 +1000

    Remove unnecessary cast

--- SRC/MANEDIT.C
+++ SRC/MANEDIT.C
@@ -984,8 +984,7 @@ int load_all_files(void)
         found = findnext(&ff);
         width++;
     }
-    qsort(_pac_files, width, sizeof(_pac_files[0]),
-          (int (*)(const void far *, const void far *)) sort_func);
+    qsort(_pac_files, width, sizeof(_pac_files[0]), sort_func);
     return width;
 }
 
